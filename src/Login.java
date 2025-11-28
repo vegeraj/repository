04f5f1587b2d2cd6ccc66e0ed39e9d321c906d3f
DESKTOP-9MT43A6\veger <vegeraj@gmail.com>
2025-11-28 10:27:53 +0100

    Login osztály létrehozása

---
+++ src/Login.java
@@ -0,0 +1,17 @@
+/**
+ * Login osztály létrehozása
+ */
+public class Login {
+    private String username;
+    private String password;
+
+    /**
+     * Konstruktor
+     * @param username
+     * @param password
+     */
+    public Login(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+}
